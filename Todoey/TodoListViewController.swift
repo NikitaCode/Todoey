2a5a2cab7e7f40c308ff0a684b9c8f9bfd6a6844
Nikita <38846642+NikitaCode@users.noreply.github.com>
2019-03-03 19:43:09 +1100

    init commit

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Nikita Sherbakov on 3/3/19.
+//  Copyright © 2019 Nikita Sherbakov. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
