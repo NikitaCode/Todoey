4d5d78bf5b6e87bfa9b5d9c9ad7d2fa89bc01d9d
Nikita <nikitush10@gmail.com>
2019-03-10 19:30:42 +1100

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Todoey
+//
+//  Created by Nikita Sherbakov on 10/3/19.
+//  Copyright © 2019 Nikita Sherbakov. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
